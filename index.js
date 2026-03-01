d65b3d634b90d162840dab3447dd5a4e69831993
XZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ <xzz556786407@gmail.com>
2026-03-01 20:59:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.get('/', (req, res) => {
-  res.send('הבוט מחובר ועובד');
-});
-
-app.get('/send', (req, res) => {
-  res.send('route send עובד');
+app.get("/", (req, res) => {
+  res.send("Server is running ✅");
 });
 
 app.listen(PORT, () => {
-  console.log(שרת פעיל על פורט ${PORT});
+  console.log("Server started on port " + PORT);
 });
