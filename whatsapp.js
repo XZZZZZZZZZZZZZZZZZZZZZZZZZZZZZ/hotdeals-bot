56d05c3813a21f597dbca3ac5972559f1f9f45bb
XZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ <xzz556786407@gmail.com>
2026-03-12 01:40:17 +0200

    Update whatsapp.js

--- whatsapp.js
+++ whatsapp.js
@@ -2,34 +2,30 @@ const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 
 const client = new Client({
-    // שימוש בתיקייה זמנית לכתיבת נתוני ההתחברות בשרת
     authStrategy: new LocalAuth({
         dataPath: '/tmp/.wwebjs_auth'
     }),
     puppeteer: {
         headless: true,
-        // התיקון הקריטי: מחפש קודם כל את המשתנה מה-Railway ואז את כרום שהתקנו ב-Nixpacks
-        executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || 'google-chrome-stable',
+        // מחקנו מכאן את השורה שחיפשה את הנתיב לדפדפן!
         args: [
             '--no-sandbox',
             '--disable-setuid-sandbox',
             '--disable-dev-shm-usage',
             '--disable-gpu'
-        ],
+        ]
     }
 });
 
-// הצגת קוד ה-QR בלוגים של Railway
 client.on('qr', (qr) => {
-    console.log('סרוק את קוד ה-QR הבא ב-View Logs:');
+    console.log('--- סרוק את קוד ה-QR המופיע כאן למטה ---');
     qrcode.generate(qr, { small: true });
 });
 
 client.on('ready', () => {
-    console.log('הוואטסאפ מחובר בהצלחה בשרת!');
+    console.log('הוואטסאפ מחובר בהצלחה בשרת החדש!');
 });
 
-// טיפול בשגיאות התחברות
 client.on('auth_failure', msg => {
     console.error('שגיאת אימות:', msg);
 });
