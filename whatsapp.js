c079bcc16e7de5be4c9acf949684cfbec842aabf
XZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ <xzz556786407@gmail.com>
2026-03-12 02:05:48 +0200

    Update whatsapp.js

--- whatsapp.js
+++ whatsapp.js
@@ -1,33 +1,35 @@
 const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 
+// הגדרות הלקוח עם דגש על חיסכון במשאבים
 const client = new Client({
-    authStrategy: new LocalAuth({
-        dataPath: '/tmp/.wwebjs_auth'
-    }),
+    authStrategy: new LocalAuth(),
     puppeteer: {
         headless: true,
-        // מחקנו מכאן את השורה שחיפשה את הנתיב לדפדפן!
         args: [
             '--no-sandbox',
             '--disable-setuid-sandbox',
             '--disable-dev-shm-usage',
-            '--disable-gpu'
-        ]
+            '--disable-gpu',
+            '--no-first-run',
+            '--no-zygote',
+            '--single-process'
+        ],
     }
 });
 
+// הדפסת QR קטן יותר - בדרך כלל נסרק הרבה יותר טוב ב-Railway
 client.on('qr', (qr) => {
-    console.log('--- סרוק את קוד ה-QR המופיע כאן למטה ---');
+    console.log('סרוק את קוד ה-QR המופיע כאן למטה:');
     qrcode.generate(qr, { small: true });
 });
 
 client.on('ready', () => {
-    console.log('הוואטסאפ מחובר בהצלחה בשרת החדש!');
+    console.log('הוואטסאפ מחובר בהצלחה!');
 });
 
-client.on('auth_failure', msg => {
-    console.error('שגיאת אימות:', msg);
+client.on('auth_failure', (msg) => {
+    console.error('שגיאת התחברות:', msg);
 });
 
 client.initialize();
