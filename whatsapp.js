34eaca90bef4fdfbde6d57322038a31afb40472e
XZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ <xzz556786407@gmail.com>
2026-03-13 01:30:02 +0200

    Update whatsapp.js

--- whatsapp.js
+++ whatsapp.js
@@ -1,20 +1,24 @@
 const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 
-// הגדרות הלקוח עם תיקוני נתיב וזמן המתנה (Timeout)
+// הגדרות הלקוח עם תיקוני נתיב, ביטול Timeout ו-Args משופרים
 const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
         headless: true,
         executablePath: '/usr/bin/google-chrome-stable',
-        protocolTimeout: 0, // מבטל את מגבלת הזמן כדי למנוע שגיאות פרוטוקול
+        protocolTimeout: 0, 
         args: [
             '--no-sandbox',
             '--disable-setuid-sandbox',
             '--disable-dev-shm-usage',
             '--disable-gpu',
             '--no-zygote',
-            '--single-process'
+            '--single-process',
+            '--hide-scrollbars',
+            '--disable-notifications',
+            '--disable-extensions',
+            '--no-first-run'
         ],
     }
 });
@@ -33,7 +37,7 @@ client.on('auth_failure', (msg) => {
     console.error('שגיאת התחברות לוואטסאפ:', msg);
 });
 
-console.log('מאתחל את הלקוח, אנא המתן לעליית הדפדפן...');
+console.log('מאתחל את הלקוח, אנא המתן לעליית הדפדפן (זה עשוי לקחת 2-3 דקות)...');
 client.initialize();
 
 module.exports = client;
